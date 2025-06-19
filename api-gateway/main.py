c5069d44e93326f8802f239e833c7ce3f7c5f248
Shiny Fernando <119037970+RominaFdo@users.noreply.github.com>
2025-06-20 03:04:13 +0530

    Thamindu merge and after the py v change

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -41,15 +41,22 @@ app = FastAPI(title="Microservices API Gateway")
 
 
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     # allow_origins=["*"],  # Frontend URL
+#     allow_origins=["http://localhost:5173"],  # Frontend URL
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Frontend URL
-    # allow_origins=["http://localhost:5173"],  # Frontend URL
+    allow_origins=["http://localhost:5173"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # User-management
 @app.post("/api/user-management/login/")

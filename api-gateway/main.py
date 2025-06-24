cb74a2cffd0c174074fa88d4620cbe4873adb8d4
Shiny Fernando <119037970+RominaFdo@users.noreply.github.com>
2025-06-24 13:30:29 +0530

    changed sevice ip in main

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -52,12 +52,12 @@ app = FastAPI(title="Microservices API Gateway")
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=[
-    # "http://localhost:3000",
-    # "http://167.71.225.152:3000"
-    # ],
-    allow_origins=["http://localhost:5173"],  # Frontend URL
-    allow_credentials=True,
+    allow_origins=[
+    "http://localhost:3000",
+    "http://167.71.225.152:3000"
+    ],
+    # allow_origins=["http://localhost:5173"],  # Frontend URL
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
